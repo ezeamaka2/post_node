b05cee9e9fcd01be3b44f140aceffdbcdc5806e5
Collins Eze <ezeamaka2@gmail.com>
2020-04-27 11:55:20 +0100

    edited readme

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,12 @@
 import { expect, server, BASE_URL } from "./setup";
 
 describe("Index page test", () => {
-  it("gets base url", (done) => {
+  it("gets all todos", (done) => {
     server
       .get(`${BASE_URL}/`)
       .expect(200)
       .end((err, res) => {
-        expect(err).to.be.null;
         expect(res.status).to.equal(200);
-        expect(res.body).to.be.an("array");
         done();
       });
   });
