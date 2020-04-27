80d4cbb172883500dc51e1b5e19d8fc0f3618329
Collins Eze <ezeamaka2@gmail.com>
2020-04-27 13:21:33 +0100

    db

--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,12 @@
 import { expect, server, BASE_URL } from "./setup";
-describe("Message", () => {
-  it("get allt odos", (done) => {
+
+describe("Index page test", () => {
+  it("gets base url", (done) => {
     server
       .get(`${BASE_URL}/`)
       .expect(200)
       .end((err, res) => {
         expect(res.status).to.equal(200);
-        expect(res.body.message).to.be.instanceOf(Array);
-        res.body.message.forEach((m) => {
-          expect(m).to.have.property("description");
-        });
         done();
       });
   });
